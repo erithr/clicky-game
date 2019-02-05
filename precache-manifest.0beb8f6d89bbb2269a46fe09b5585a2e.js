60db1ce74fea9e3ad1fb83f5a573b1a9991dd2ec
erithr <hvillalo@linkedin.biz>
2019-02-04 20:28:59 -0600

    Updates

---
+++ precache-manifest.0beb8f6d89bbb2269a46fe09b5585a2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe992d88e7524edc0f93",
+    "url": "/cliky-game/static/js/runtime~main.fe992d88.js"
+  },
+  {
+    "revision": "212dfc09ed396ea4d47a",
+    "url": "/cliky-game/static/js/main.212dfc09.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/cliky-game/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "212dfc09ed396ea4d47a",
+    "url": "/cliky-game/static/css/main.de60db22.chunk.css"
+  },
+  {
+    "revision": "0e32d128c2c5ab5c7b11ba91fab1d55c",
+    "url": "/cliky-game/index.html"
+  }
+];
