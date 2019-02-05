080a29c1501612d6fe5d6d944a81c0175ca7cbe4
erithr <hvillalo@linkedin.biz>
2019-02-04 19:11:24 -0600

    multiple addons and changes

---
+++ clicky-game/src/App.js
@@ -0,0 +1,107 @@
+import React, { Component } from "react";
+import FriendCard from "./components/FriendCard";
+import Nav from "./components/Nav";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import Container from "./components/Container";
+import Row from "./components/Row";
+import Column from "./components/Column/Column";
+import friends from "./friends.json";
+import "./App.css";
+
+
+// Random shuffle
+function randomFriends(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
+
+class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    friends,
+    currentScore: 0,
+    topScore: 0,
+    correctIncorrect: "",
+    clicked: [],
+  };
+
+  handleClick = id => {
+    if (this.state.clicked.indexOf(id) === -1) {
+      this.handleIncrement();
+      this.setState({ clicked: this.state.clicked.concat(id) });
+    } else {
+      this.handleReset();
+    }
+  };
+
+  handleIncrement = () => {
+    const newScore = this.state.currentScore + 1;
+    this.setState({
+      currentScore: newScore,
+      correctIncorrect: "You guessed correctly!"
+    });
+    if (newScore >= this.state.topScore) {
+      this.setState({ topScore: newScore });
+    }
+    else if (newScore === 12) {
+      this.setState({ correctIncorrect: "You win!" });
+    }
+    this.handleShuffle();
+  };
+
+  handleReset = () => {
+    this.setState({
+      currentScore: 0,
+      topScore: this.state.topScore,
+      correctIncorrect: "You guessed incorrectly!",
+      clicked: []
+    });
+    this.handleShuffle();
+  };
+
+  handleShuffle = () => {
+    let shuffledFriends = randomFriends(friends);
+    this.setState({ friends: shuffledFriends });
+  };
+
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
+  render() {
+    return (
+      <Wrapper>
+        <Nav
+          title="React Clicky Game"
+          score={this.state.currentScore}
+          topScore={this.state.topScore}
+          correctIncorrect={this.state.correctIncorrect}
+        />
+
+        <Title>
+          Click on an image to earn points, but don't click on any more than once!
+        </Title>
+        <Container>
+          <Row>
+            {this.state.friends.map(friend => (
+              <Column size="md-3 sm-6">
+                <FriendCard
+                  key={friend.id}
+                  handleClick={this.handleClick}
+                  handleIncrement={this.handleIncrement}
+                  handleReset={this.handleReset}
+                  handleShuffle={this.handleShuffle}
+                  id={friend.id}
+                  image={friend.image}
+                />
+              </Column>
+            ))}
+          </Row>
+        </Container>
+      </Wrapper>
+    );
+  }
+}
+export default App;
